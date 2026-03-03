6fd8fdf1912e85bdadd9d1b8b3c0aae5065f6a6b
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-03 20:24:32 +0530

    Added solution - LeetHub

---
+++ 703213-find-element-at-a-given-index-GFG/703213-find-element-at-a-given-index.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+    public static int findElementAtIndex(int key, int[] arr) {
+        // code here
+        
+        int ans = arr[key];
+        
+        return ans;
+    }
+}

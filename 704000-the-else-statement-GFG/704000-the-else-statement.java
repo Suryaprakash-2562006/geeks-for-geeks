9361d0022a8aa2fadb76fd3fa95d8e4ea1fbfdfb
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-05 22:40:06 +0530

    Added solution - LeetHub

---
+++ 704000-the-else-statement-GFG/704000-the-else-statement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static void utility(int number) {
+        if(number > 100)
+        {
+            System.out.println("Big");
+        }
+        else
+        {
+            System.out.println("Number");
+        }
+    }
+}

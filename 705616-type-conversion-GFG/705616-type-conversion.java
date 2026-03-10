bbd1dd5e85bb2a28d62c473b5697de085c4fe722
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-10 21:34:25 +0530

    Added solution - LeetHub

---
+++ 705616-type-conversion-GFG/705616-type-conversion.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        // Typecast and print
+        
+        int c = (int) d;
+        
+        System.out.print(c);
+    }
+}

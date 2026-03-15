7821c38d28afa08ecf75dfc39205f60df55f86c2
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-15 21:20:05 +0530

    Added solution - LeetHub

---
+++ 878875-greatest-of-three-GFG/878875-greatest-of-three.java
@@ -0,0 +1,18 @@
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your code here
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        
+        int ans = (a>b && a>c) ? a : (b>c) ? b : c;
+        
+        System.out.print(ans);
+    }
+}

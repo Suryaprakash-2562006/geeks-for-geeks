7767279d7cdabf9db4cd42c8241dd5e8aa998ee9
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-17 20:59:28 +0530

    Added solution - LeetHub

--- 878917-gcd-GFG/878917-gcd.java
+++ 878917-gcd-GFG/878917-gcd.java
@@ -1,42 +1,13 @@
 class Solution {
     public int computeGCD(int a, int b) {
-        // code here
-        int gcd = 0;
-        
-        if(a==1 || b==1)
-        {
-           gcd = 1;
-        }
-        
-        else if(a==b)
-        {
-           gcd = a;
-        }
-        
-        
-        for(int i=2;i<=Math.min(a,b);i++)
-        {
-            
-            if(a==1 || b==1)
-            {
-              break;
+        int gcd = 1;
+
+        for(int i = 1; i <= Math.min(a, b); i++) {
+            if(a % i == 0 && b % i == 0) {
+                gcd = i; // keep updating
             }
-            
-            else if(a==b)
-            {
-              break;
-            }
-            
-            
-            
-            if(a%i == 0 && b%i==0)
-            {
-                gcd = i;
-                break;
-            }
-            
         }
-        
+
         return gcd;
     }
 }

17719720e1a22ed705e31ee923b520d1b8e5462b
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-23 21:59:25 +0530

    Added solution - LeetHub

--- 703089-second-largest-GFG/703089-second-largest.java
+++ 703089-second-largest-GFG/703089-second-largest.java
@@ -1,20 +1,25 @@
 class Solution {
     public int getSecondLargest(int[] arr) {
+        // code here
         
         int large = arr[0];
         int slarge = -1;
         
-        for(int i = 1; i < arr.length; i++) {
-            
-            if(arr[i] > large) {
-                slarge = large;   // store previous largest
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i] > large){
+                slarge = large;
                 large = arr[i];
             }
-            else if(arr[i] < large && arr[i] > slarge) {
+            
+            else if(arr[i] < large && arr[i] > slarge)
+            {
                 slarge = arr[i];
             }
         }
         
         return slarge;
+        
+        
     }
 }

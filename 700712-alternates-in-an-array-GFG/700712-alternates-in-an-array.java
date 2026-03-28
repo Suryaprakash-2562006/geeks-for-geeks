2848115a689359025b60b4926b8a7f648fb9d72a
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-28 21:48:24 +0530

    Added solution - LeetHub

---
+++ 700712-alternates-in-an-array-GFG/700712-alternates-in-an-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        // Code Here
+        
+        ArrayList<Integer> ar = new ArrayList<>();
+        for(int i=0;i<arr.length;i+=2){
+            ar.add(arr[i]);
+            
+        }
+        
+        return ar;
+    }
+}

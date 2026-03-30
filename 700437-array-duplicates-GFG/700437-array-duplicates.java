5f09a7557836a4bd7873b0758e361a1eb54be9f0
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-30 20:27:42 +0530

    Added solution - LeetHub

---
+++ 700437-array-duplicates-GFG/700437-array-duplicates.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public ArrayList<Integer> findDuplicates(int[] arr) 
+    {
+        ArrayList<Integer> dup = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            int index = Math.abs(arr[i]) - 1;
+            
+            if(arr[index] < 0)
+            {
+                dup.add(index + 1);
+            }
+            else
+            {
+                arr[index] = -arr[index];
+            }
+        }
+        
+        return dup;
+    }
+}

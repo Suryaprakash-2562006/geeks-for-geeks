340ee0fcbe8f09549c01555e42f1ed2563480d03
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-03-31 21:49:58 +0530

    Added solution - LeetHub

---
+++ 700269-replace-all-0s-with-5-GFG/700269-replace-all-0s-with-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    int convertfive(int num) {
+        // Your code here
+        
+        int rev = 0;
+        int r = 0;
+        
+        if(num == 0){
+            r = 5;
+        }
+        
+        while(num > 0){
+            int n = num % 10;
+            if(n == 0)
+            {
+                n = 5;
+            }
+            
+            rev = rev * 10 + n;
+            
+            num /= 10;
+        }
+        
+        
+        
+        while(rev > 0){
+            int n = rev % 10;
+            
+            r = r * 10 + n;
+            
+            rev /= 10;
+        }
+        
+        return r;
+        
+        
+    }
+}
+

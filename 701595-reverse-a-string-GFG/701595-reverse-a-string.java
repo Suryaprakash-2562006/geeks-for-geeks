14a2e51aab7b51e911d8d2086cb47b352ff9e455
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-04-04 20:40:59 +0530

    Added solution - LeetHub

---
+++ 701595-reverse-a-string-GFG/701595-reverse-a-string.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    public static String reverseString(String s) {
+        // code here
+        if(s.length() == 1)
+        {
+            return s;
+            
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=s.length()-1;i>=0;i--){
+            sb.append(s.charAt(i));
+        }
+        
+        return sb.toString();
+        
+        
+    }
+}

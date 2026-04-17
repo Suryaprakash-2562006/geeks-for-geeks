8f298587296c37a039ebb9353aec3e95f40fe90e
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-04-17 23:47:21 +0530

    Added solution - LeetHub

---
+++ 705350-arithmetic-operators-GFG/705350-arithmetic-operators.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public static void operations(int x, int y) {
+        // Perform addition
+        int p = x + y;
+
+        // Perform subtraction
+        int q = x - y;
+
+        // Perform multiplication
+        int r = x * y;
+
+        // Perform division (integer division)
+        int s = x / y;
+
+        // Perform modulo
+        int t = x % y;
+
+        // Don't change this
+        System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+}
